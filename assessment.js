435b232516b2173d3b85f9d77fbdd619a33dea0d
Matthew V <35playfun@gmail.com>
2026-06-26 16:39:44 -0400

    create and link javascript file; create testing assessment data

---
+++ assessment.js
@@ -0,0 +1,22 @@
+assessmentData = {
+    "questions": [
+        {
+            "type": "multiple-choice",
+            "question": "What color is the sky on a clear day?",
+            "options": ["Blue", "Green", "Red", "Yellow"],
+            "correctAnswers": ["Blue"]
+        },
+        {
+            "type": "multiple-choice",
+            "question": "What time is noon?",
+            "options": ["12:00 AM", "12:00 PM", "6:00 AM", "6:00 PM"],
+            "correctAnswers": ["12:00 PM"]
+        },
+        {
+            "type": "short-answer",
+            "question": "What is an example of a primary color?",
+            "correctAnswers": ["red", "blue", "yellow"]
+        },
+    ]
+}
+
